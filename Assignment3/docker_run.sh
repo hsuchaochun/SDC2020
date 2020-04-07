f186aacebe228e12b0b1f432752cb95666e3e4e1
leohsu870325 <leohsu210200@gmail.com>
2020-04-07 23:52:34 +0800

    Revise docker run file

--- Assignment3/docker_run.sh
+++ Assignment3/docker_run.sh
@@ -1,5 +1,5 @@
 if [ "$1" = "run" ]; then
-    docker run -it --net host --rm -v "${PWD}/catkin_ws:/catkin_ws" --name sdc osrf/ros:kinetic-desktop-full
+    docker run -it --net host --rm -v "${PWD}/catkin_ws:/catkin_ws" --name sdc sdc2020:latest
 else
     docker exec -it sdc bash
 fi
